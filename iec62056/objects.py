b6d5d51790712448ae7363a841e95aacf9798745
Ioannis Papamanoglou <ioannis.papamanoglou@zonneplan.nl>
2023-01-10 14:45:31 +0100

    Improve parsing & Autoformat (accidentally)
    - Multiple telegrams
    - Ignore broken registers (only throw logger warnings)
    - Non standard dsmr headers
    
    - Format with black

--- iec62056/objects.py
+++ iec62056/objects.py
@@ -1,152 +1,177 @@
 import datetime
+import logging
 import time
 
+
 class Reference(object):
-	def __init__(self, i):
-		self.id = i
-	
-	def __repr__(self):
-		return '{}-{}:{}.{}.{}'.format(*self.id)
-	
-	def match(self, other):
-		# length mismatch
-		if len(other.id) != len(self.id):
-			return False
-		
-		# run every digit
-		for digit_self, digit_other in [(str(self.id[i]), str(other.id[i])) for i in range(len(self.id))]:
-			# digits don't match directly
-			if digit_self != digit_other:
-				# if neither is wildcard, mismatch
-				if digit_self != '*' and digit_other != '*':
-					return False
-		return True
+    def __init__(self, i):
+        self.id = i
+
+    def __repr__(self):
+        return "{}-{}:{}.{}.{}".format(*self.id)
+
+    def match(self, other):
+        # length mismatch
+        if len(other.id) != len(self.id):
+            return False
+
+        # run every digit
+        for digit_self, digit_other in [
+            (str(self.id[i]), str(other.id[i])) for i in range(len(self.id))
+        ]:
+            # digits don't match directly
+            if digit_self != digit_other:
+                # if neither is wildcard, mismatch
+                if digit_self != "*" and digit_other != "*":
+                    return False
+        return True
+
 
 class COSEM(object):
-	def __init__(self, reference, name):
-		self.reference = reference
-		self.name = name
-	
-class Log(COSEM):
-	def __init__(self, reference, name, entries):
-		super().__init__(reference, name)
-		self.entries = entries
+    def __init__(self, reference, name):
+        self.reference = reference
+        self.name = name
 
-	@classmethod
-	def factory(cls, reference, entries):
-		for r, n, func in cosem_objects:
-			if r.match(reference):
-				name = n
-				return cls(reference, name, entries)
-		raise NotImplementedError('OBIS reference {} is not implemented'.format(repr(reference)))
-	
-	def __repr__(self):
-		return '<Log reference={} name={} [{}]>'.format(self.reference, self.name, self.entries)
+
+class Log(COSEM):
+    def __init__(self, reference, name, entries):
+        super().__init__(reference, name)
+        self.entries = entries
+
+    @classmethod
+    def factory(cls, reference, entries):
+        for r, n, func in cosem_objects:
+            if r.match(reference):
+                name = n
+                return cls(reference, name, entries)
+        raise NotImplementedError(
+            "OBIS reference {} is not implemented".format(repr(reference))
+        )
+
+    def __repr__(self):
+        return "<Log reference={} name={} [{}]>".format(
+            self.reference, self.name, self.entries
+        )
+
 
 class Register(COSEM):
-	def __init__(self, reference, name, timestamp, value, unit):
-		super().__init__(reference, name)
-		self.timestamp = timestamp
-		self.value = value
-		self.unit = unit
-	
-	@classmethod
-	def factory(cls, reference, timestamp, raw, unit):
-		for r, n, vcls in cosem_objects:
-			if r.match(reference):
-				value = vcls(raw)
-				name = n
-				return cls(reference, name, timestamp, value, unit)
-		raise NotImplementedError('OBIS reference {} is not implemented'.format(repr(reference)))
-	
-	def __repr__(self):
-		if self.timestamp is not None:
-			t = self.timestamp.timestamp()
-		else:
-			t = None
-		return '<Register reference={} name={} timestamp={} value={} unit={}>'.format(self.reference, self.name, t, self.value, self.unit)
-	
-	def __int__(self):
-		return int(self.value)
+    def __init__(self, reference, name, timestamp, value, unit):
+        super().__init__(reference, name)
+        self.timestamp = timestamp
+        self.value = value
+        self.unit = unit
 
-	def __str__(self):
-		return str(self.value)
+    @classmethod
+    def factory(cls, reference, timestamp, raw, unit):
+        for r, n, vcls in cosem_objects:
+            if r.match(reference):
+                try:
+                    value = vcls(raw)
+                except TypeError:
+                    logging.warning(
+                        f"Invalid register: {cls, reference, timestamp, raw, unit}"
+                    )
+                    return None
+                name = n
+                return cls(reference, name, timestamp, value, unit)
+        logging.warning("OBIS reference {} is not implemented".format(repr(reference)))
+        return None
 
-	def __float__(self):
-		return float(self.value)
+    def __repr__(self):
+        if self.timestamp is not None:
+            t = self.timestamp.timestamp()
+        else:
+            t = None
+        return "<Register reference={} name={} timestamp={} value={} unit={}>".format(
+            self.reference, self.name, t, self.value, self.unit
+        )
+
+    def __int__(self):
+        return int(self.value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __float__(self):
+        return float(self.value)
+
 
 class Telegram(object):
-	def __init__(self, identification, objects, checksum):
-		self.vendor, self.version, self.model = identification
-		self.objects = objects
-		self.checksum = checksum
-	
-	def  __repr__(self):
-		return '<Telegram {}/{} containing {} checksum={}>'.format(self.vendor, self.model, ','.join(self.keys()), self.checksum)
-	
-	def keys(self):
-		return [o.name for o in self.objects]
-	
-	def __getitem__(self, name):
-		for o in self.objects:
-			if o.name == name:
-				return o
-		raise ValueError('key "{}" not in telegram'.format(name))
+    def __init__(self, identification, objects, checksum):
+        self.vendor, self.version, self.model = identification
+        self.objects = objects
+        self.checksum = checksum
+
+    def __repr__(self):
+        return "<Telegram {}/{} containing {} checksum={}>".format(
+            self.vendor, self.model, ",".join(self.keys()), self.checksum
+        )
+
+    def keys(self):
+        return [o.name for o in self.objects]
+
+    def __getitem__(self, name):
+        for o in self.objects:
+            if o.name == name:
+                return o
+        raise ValueError('key "{}" not in telegram'.format(name))
+
 
 def timestamp(raw):
-	# cosem timestamp format = YYMMDDhhmmssX
-	# we ditch the dst flag (S, summer, dst=True, W, winter, dst=False)
-	# since we strptime according to local timezone
-	dt = datetime.datetime.strptime(raw[:12], '%y%m%d%H%M%S')
-	zone = datetime.datetime.now(datetime.timezone.utc).astimezone()
-	return dt.replace(tzinfo=zone.tzinfo)
+    # cosem timestamp format = YYMMDDhhmmssX
+    # we ditch the dst flag (S, summer, dst=True, W, winter, dst=False)
+    # since we strptime according to local timezone
+    dt = datetime.datetime.strptime(raw[:12], "%y%m%d%H%M%S")
+    zone = datetime.datetime.now(datetime.timezone.utc).astimezone()
+    return dt.replace(tzinfo=zone.tzinfo)
+
 
 cosem_objects = [
-# These are from various DSMR specs
-	(Reference((0, 0, 1, 0, 0)),                  'timestamp', timestamp),
-	(Reference((0, 0, 17, 0, 0)),                 'threshold_electricity', float),
-	(Reference((0, 0, 96, 1, 1)),                 'equipment_identifier', str),
-	(Reference((0, 0, 96, 3, 10)),                'electricity_switch_position', int),
-	(Reference((0, 0, 96, 7, 19)),                'power_failure_duration', int),
-	(Reference((0, 0, 96, 7, 21)),                'short_power_failure_count', int),
-	(Reference((0, 0, 96, 7, 9)),                 'long_power_failure_count', int),
-	(Reference((0, 0, 96, 13, 0)),                'text_message', str),
-	(Reference((0, 0, 96, 13, 1)),                'text_message_code', str),
-	(Reference((0, 0, 96, 14, 0)),                'electricity_active_tariff', int),
-	(Reference((0, '*', 24, 1, 0)),               'device_type', int),
-	(Reference((0, '*', 24, 2, 1)),               'gas_delivered', float),
-	(Reference((0, '*', 24, 3, 0)),               'gas_delivered', float),
-	(Reference((0, '*', 24, 4, 0)),               'gas_valve_position', int),
-	(Reference((0, '*', 96, 1, 0)),               'equipment_identifier', str),
-	(Reference((1, 0, 1, 7, 0)),                  'current_electricity_usage', float),
-	(Reference((1, 0, 1, 8, 1)),                  'electricity_used_tariff_1', float),
-	(Reference((1, 0, 1, 8, 2)),                  'electricity_used_tariff_2', float),
-	(Reference((1, 0, 2, 7, 0)),                  'current_electricity_delivery', float),
-	(Reference((1, 0, 2, 8, 1)),                  'electricity_delivered_tariff_1', float),
-	(Reference((1, 0, 2, 8, 2)),                  'electricity_delivered_tariff_2', float),
-	(Reference((1, 0, 21, 7, 0)),                 'instantaneous_active_power_l1_positive', float),
-	(Reference((1, 0, 22, 7, 0)),                 'instantaneous_active_power_l1_negative', float),
-	(Reference((1, 0, 31, 7, 0)),                 'instantaneous_current_l1', float),
-	(Reference((1, 0, 32, 7, 0)),                 'instantaneous_voltage_l1', float),
-	(Reference((1, 0, 32, 32, 0)),                'voltage_sag_count_l1', int),
-	(Reference((1, 0, 32, 36, 0)),                'voltage_swell_count_l1', int),
-	(Reference((1, 0, 41, 7, 0)),                 'instantaneous_active_power_l2_positive', float),
-	(Reference((1, 0, 42, 7, 0)),                 'instantaneous_active_power_l2_negative', float),
-	(Reference((1, 0, 51, 7, 0)),                 'instantaneous_current_l2', float),
-	(Reference((1, 0, 52, 7, 0)),                 'instantaneous_voltage_l2', float),
-	(Reference((1, 0, 52, 32, 0)),                'voltage_sag_count_l2', int),
-	(Reference((1, 0, 52, 36, 0)),                'voltage_swell_count_l2', int),
-	(Reference((1, 0, 61, 7, 0)),                 'instantaneous_active_power_l3_positive', float),
-	(Reference((1, 0, 62, 7, 0)),                 'instantaneous_active_power_l3_negative', float),
-	(Reference((1, 0, 71, 7, 0)),                 'instantaneous_current_l3', float),
-	(Reference((1, 0, 72, 7, 0)),                 'instantaneous_voltage_l3', float),
-	(Reference((1, 0, 72, 32, 0)),                'voltage_sag_count_l3', int),
-	(Reference((1, 0, 72, 36, 0)),                'voltage_swell_count_l3', int),
-	(Reference((1, 0, 99, 97, 0)),                'power_failure_event_log', None),
-	(Reference((1, 3, 0, 2, 8)),                  'version', str),
-# These are reverse-engineered Kamstrup multical fields
-	(Reference((None, None, 0, 0, None)),         'serial', str),
-	(Reference((None, None, 6, 8, None)),         'energy_consumed', float),
-	(Reference((None, None, 6, 26, None)),        'volume_consumed', float),
-	(Reference((None, None, 6, 31, None)),        'operating_hours', int),
+    # These are from various DSMR specs
+    (Reference((0, 0, 1, 0, 0)), "timestamp", timestamp),
+    (Reference((0, 0, 17, 0, 0)), "threshold_electricity", float),
+    (Reference((0, 0, 96, 1, 1)), "equipment_identifier", str),
+    (Reference((0, 0, 96, 3, 10)), "electricity_switch_position", int),
+    (Reference((0, 0, 96, 7, 19)), "power_failure_duration", int),
+    (Reference((0, 0, 96, 7, 21)), "short_power_failure_count", int),
+    (Reference((0, 0, 96, 7, 9)), "long_power_failure_count", int),
+    (Reference((0, 0, 96, 13, 0)), "text_message", str),
+    (Reference((0, 0, 96, 13, 1)), "text_message_code", str),
+    (Reference((0, 0, 96, 14, 0)), "electricity_active_tariff", int),
+    (Reference((0, "*", 24, 1, 0)), "device_type", int),
+    (Reference((0, "*", 24, 2, 1)), "gas_delivered", float),
+    (Reference((0, "*", 24, 3, 0)), "gas_delivered", float),
+    (Reference((0, "*", 24, 4, 0)), "gas_valve_position", int),
+    (Reference((0, "*", 96, 1, 0)), "equipment_identifier", str),
+    (Reference((1, 0, 1, 7, 0)), "current_electricity_usage", float),
+    (Reference((1, 0, 1, 8, 1)), "electricity_used_tariff_1", float),
+    (Reference((1, 0, 1, 8, 2)), "electricity_used_tariff_2", float),
+    (Reference((1, 0, 2, 7, 0)), "current_electricity_delivery", float),
+    (Reference((1, 0, 2, 8, 1)), "electricity_delivered_tariff_1", float),
+    (Reference((1, 0, 2, 8, 2)), "electricity_delivered_tariff_2", float),
+    (Reference((1, 0, 21, 7, 0)), "instantaneous_active_power_l1_positive", float),
+    (Reference((1, 0, 22, 7, 0)), "instantaneous_active_power_l1_negative", float),
+    (Reference((1, 0, 31, 7, 0)), "instantaneous_current_l1", float),
+    (Reference((1, 0, 32, 7, 0)), "instantaneous_voltage_l1", float),
+    (Reference((1, 0, 32, 32, 0)), "voltage_sag_count_l1", int),
+    (Reference((1, 0, 32, 36, 0)), "voltage_swell_count_l1", int),
+    (Reference((1, 0, 41, 7, 0)), "instantaneous_active_power_l2_positive", float),
+    (Reference((1, 0, 42, 7, 0)), "instantaneous_active_power_l2_negative", float),
+    (Reference((1, 0, 51, 7, 0)), "instantaneous_current_l2", float),
+    (Reference((1, 0, 52, 7, 0)), "instantaneous_voltage_l2", float),
+    (Reference((1, 0, 52, 32, 0)), "voltage_sag_count_l2", int),
+    (Reference((1, 0, 52, 36, 0)), "voltage_swell_count_l2", int),
+    (Reference((1, 0, 61, 7, 0)), "instantaneous_active_power_l3_positive", float),
+    (Reference((1, 0, 62, 7, 0)), "instantaneous_active_power_l3_negative", float),
+    (Reference((1, 0, 71, 7, 0)), "instantaneous_current_l3", float),
+    (Reference((1, 0, 72, 7, 0)), "instantaneous_voltage_l3", float),
+    (Reference((1, 0, 72, 32, 0)), "voltage_sag_count_l3", int),
+    (Reference((1, 0, 72, 36, 0)), "voltage_swell_count_l3", int),
+    (Reference((1, 0, 99, 97, 0)), "power_failure_event_log", None),
+    (Reference((1, 3, 0, 2, 8)), "version", str),
+    # These are reverse-engineered Kamstrup multical fields
+    (Reference((None, None, 0, 0, None)), "serial", str),
+    (Reference((None, None, 6, 8, None)), "energy_consumed", float),
+    (Reference((None, None, 6, 26, None)), "volume_consumed", float),
+    (Reference((None, None, 6, 31, None)), "operating_hours", int),
 ]
--- iec62056/parser.py
+++ iec62056/parser.py
@@ -4,117 +4,136 @@ import os.path
 from lark import Lark, Transformer
 import iec62056.objects
 
+
 class IECTransformer(Transformer):
-	def vendor(self, tree):
-		return ''.join(tree)
-	
-	def model(self, tree):
-		return ''.join(tree)
-	
-	def version(self, tree):
-		if len(tree) > 0:
-			return str(tree[0])
-		else:
-			return None
-	
-	def header_iec(self, tree):
-		return (tree[0], tree[1], tree[2])
-	
-	def header_dsmr(self, tree):
-		return (tree[0], tree[1], tree[2])
-	
-	def footer_iec(self, tree):
-		return tree[0]
-	
-	def footer_dsmr(self, tree):
-		if len(tree) > 0:
-			return tree[0]
-		else:
-			return None
-	
-	def checksum1(self, tree):
-		return ord(tree[0][0])
-	
-	def checksum4(self, tree):
-		h = ''.join(tree[0:4])
-		return int(h, 16)
-	
-	def id_abcde(self, tree):
-		return iec62056.objects.Reference(tuple(map(int, tree)))
+    def vendor(self, tree):
+        return "".join(tree)
 
-	def id_cd(self, tree):
-		return iec62056.objects.Reference((None, None, int(tree[0]), int(tree[1]), None))
-	
-	def value(self, tree):
-		return str(tree[0])
-	
-	def unit(self, tree):
-		return str(tree[0])
-	
-	def cosem_value_unit(self, tree):
-		return (tree[0], tree[1])
-	
-	def cosem_value(self, tree):
-		return (tree[0], None)
-	
-	def cosem_empty(self, tree):
-		return (None, None)
-	
-	def timestamp(self, tree):
-		# cosem timestamp format = YYMMDDhhmmssX
-		# tree[0] = YYMMDDhhmmss
-		# tree[1] = X (DST)
-		if len(tree) == 2:
-			return iec62056.objects.timestamp(tree[0] + tree[1])
-		else:
-			return iec62056.objects.timestamp(tree[0])
-	
-	def register(self, tree):
-		reference, (value, unit) = tree
-		return iec62056.objects.Register.factory(reference, None, value, unit)
-	
-	def mbus(self, tree):
-		return (tree[0], tree[1])
-	
-	def timestamp_register(self, tree):
-		reference, (timestamp, (value, unit)) = tree
-		return iec62056.objects.Register.factory(reference, timestamp, value, unit)
-	
-	def logentry(self, tree):
-		return (tree[0], tree[1][0], tree[1][1])
+    def model(self, tree):
+        return "".join(tree)
 
-	def profilegeneric(self, tree):
-		n = int(tree.pop(0))
-		reference = tree.pop(0)
-		entries = tree
-		#assert len(entries) == n
-		return [iec62056.objects.Register.factory(reference, timestamp, value, unit) for timestamp, value, unit in entries]
-	
-	def log(self, tree):
-		reference = tree.pop(0)
-		entries = tree
-		return iec62056.objects.Log.factory(reference, entries)
-	
-	def dsmr3_gas(self, tree):
-		return (tree[0], tree[4], tree[5], tree[6])
-	
-	def dsmr3_gas_register(self, tree):
-		register, (timestamp, reg, unit, value) = tree
-		return iec62056.objects.Register.factory(reg, timestamp, value, unit)
-	
-	def objects(self, tree):
-		return tree
-	
-	def telegram(self, tree):
-		return iec62056.objects.Telegram(tree[0], tree[1], tree[2])
+    def version(self, tree):
+        if len(tree) > 0:
+            return str(tree[0])
+        else:
+            return None
+
+    def header_iec(self, tree):
+        return (tree[0], tree[1], tree[2])
+
+    def header_dsmr(self, tree):
+        return tree[-1]
+
+    def header_id_standard(self, tree):
+        return (tree[0], tree[1], tree[2])
+
+    def header_id_vendor(self, tree):
+        return ("N/A", "N/A", "".join(tree))
+
+    def header_id(self, tree):
+        return tree[0]
+
+    def footer_iec(self, tree):
+        return tree[0]
+
+    def footer_dsmr(self, tree):
+        if len(tree) > 0:
+            return tree[0]
+        else:
+            return None
+
+    def checksum1(self, tree):
+        return ord(tree[0][0])
+
+    def checksum4(self, tree):
+        h = "".join(tree[0:4])
+        return int(h, 16)
+
+    def id_abcde(self, tree):
+        return iec62056.objects.Reference(tuple(map(int, tree)))
+
+    def id_cd(self, tree):
+        return iec62056.objects.Reference(
+            (None, None, int(tree[0]), int(tree[1]), None)
+        )
+
+    def value(self, tree):
+        return str(tree[0])
+
+    def unit(self, tree):
+        return str(tree[0])
+
+    def cosem_value_unit(self, tree):
+        return (tree[0], tree[1])
+
+    def cosem_value(self, tree):
+        return (tree[0], None)
+
+    def cosem_empty(self, tree):
+        return (None, None)
+
+    def timestamp(self, tree):
+        # cosem timestamp format = YYMMDDhhmmssX
+        # tree[0] = YYMMDDhhmmss
+        # tree[1] = X (DST)
+        if len(tree) == 2:
+            return iec62056.objects.timestamp(tree[0] + tree[1])
+        else:
+            return iec62056.objects.timestamp(tree[0])
+
+    def register(self, tree):
+        reference, (value, unit) = tree
+        return iec62056.objects.Register.factory(reference, None, value, unit)
+
+    def mbus(self, tree):
+        return (tree[0], tree[1])
+
+    def timestamp_register(self, tree):
+        reference, (timestamp, (value, unit)) = tree
+        return iec62056.objects.Register.factory(reference, timestamp, value, unit)
+
+    def logentry(self, tree):
+        return (tree[0], tree[1][0], tree[1][1])
+
+    def profilegeneric(self, tree):
+        n = int(tree.pop(0))
+        reference = tree.pop(0)
+        entries = tree
+        # assert len(entries) == n
+        return [
+            iec62056.objects.Register.factory(reference, timestamp, value, unit)
+            for timestamp, value, unit in entries
+        ]
+
+    def log(self, tree):
+        reference = tree.pop(0)
+        entries = tree
+        return iec62056.objects.Log.factory(reference, entries)
+
+    def dsmr3_gas(self, tree):
+        return (tree[0], tree[4], tree[5], tree[6])
+
+    def dsmr3_gas_register(self, tree):
+        register, (timestamp, reg, unit, value) = tree
+        return iec62056.objects.Register.factory(reg, timestamp, value, unit)
+
+    def objects(self, tree):
+        return list(filter(lambda x: x is not None, tree))
+
+    def telegram(self, tree):
+        return iec62056.objects.Telegram(tree[0], tree[1], tree[2])
+
+    def telegrams(self, tree):
+        return tree
+
 
 class Parser(object):
-	def __init__(self):
-		this_dir, this_filename = os.path.split(__file__)
-		grammar_file = os.path.join(this_dir, 'grammar.lark')
-		self.parser = Lark.open(grammar_file, start='telegram')
-		self.transformer = IECTransformer()
-	
-	def parse(self, data):
-		tree = self.parser.parse(data)
-		return self.transformer.transform(tree)
+    def __init__(self):
+        this_dir, this_filename = os.path.split(__file__)
+        grammar_file = os.path.join(this_dir, "grammar.lark")
+        self.parser = Lark.open(grammar_file, start="telegrams")
+        self.transformer = IECTransformer()
+
+    def parse(self, data):
+        tree = self.parser.parse(data)
+        return self.transformer.transform(tree)
